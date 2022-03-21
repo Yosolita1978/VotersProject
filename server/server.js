2567e9226a098d4c527a29b8280253f53acba0f2
Cristina Rodriguez <yosola@gmail.com>
2022-03-21 15:08:58 -0700

    Connection to DB voters working in the backend

---
+++ server/server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const db = require('../server/db/db-connection.js'); 
+
+const app = express();
+
+const PORT = 5000;
+app.use(cors());
+app.use(express.json());
+
+//creates an endpoint for the route /api
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello from My ExpressJS' });
+});
+
+//create the get request
+app.get('/api/voters', cors(), async (req, res) => {
+    // const VOTERS = [
+
+    //     { id: 1, commonname: 'Elefante', scientificname: 'Latinus', numbertinthewild: 200  },
+    // // ];
+    // res.json(VOTERS);
+    try{
+        const { rows: voters } = await db.query('SELECT * FROM voters');
+        res.send(voters);
+    } catch (e){
+        return res.status(400).json({e});
+    }
+});
+
+//create the POST request
+// app.post('/api/students', cors(), async (req, res) => {
+//     const newUser = { firstname: req.body.firstname, lastname: req.body.lastname }
+//     console.log([newUser.firstname, newUser.lastname]);
+//     const result = await db.query(
+//         'INSERT INTO students(firstname, lastname) VALUES($1, $2) RETURNING *',
+//         [newUser.firstname, newUser.lastname]
+//     );
+//     console.log(result.rows[0]);
+//     res.json(result.rows[0]);
+// });
+
+// console.log that your server is up and running
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+});
